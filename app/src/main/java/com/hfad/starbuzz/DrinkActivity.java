cc6ba7ccbbfcff9354b81b248142bd9f6a373491
Steve Lee <yongdori@gmail.com>
2020-02-10 23:18:37 -0500

    Chapter16 Completed Implemented cursor to read data from SQLite DB

--- app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -2,14 +2,19 @@ package com.hfad.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -29,7 +34,7 @@ public class DrinkActivity extends AppCompatActivity {
         try{
             SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
             Cursor cursor = db.query("DRINK",
-                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID", "FAVORITE"},
                     "_id = ?",
                     new String[]{Integer.toString(drinkId)},
                     null, null, null);
@@ -41,6 +46,7 @@ public class DrinkActivity extends AppCompatActivity {
                 String nameText = cursor.getString(0);
                 String descriptionText = cursor.getString(1);
                 int photoId = cursor.getInt(2);
+                boolean isFavorite = (cursor.getInt(3) == 1);
 
                 //Populate the drink name
                 TextView name = (TextView)findViewById(R.id.name);
@@ -59,6 +65,10 @@ public class DrinkActivity extends AppCompatActivity {
                 photo.setImageResource(photoId);
                 photo.setContentDescription(nameText);
 
+                //Populate the favorite checkbox
+                CheckBox favorite = (CheckBox)findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
+
             }
             cursor.close();
             db.close();
@@ -68,5 +78,48 @@ public class DrinkActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    //Update the database when the checkbox is clicked
+    public void onFavoriteClicked(View view){
+        int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
+        new UpdateDrinkTask().execute(drinkId);
+
+    }
+
+    //Inner class to update the drink.
+    private class UpdateDrinkTask extends AsyncTask<Integer, Void, Boolean>{
+        private ContentValues drinkValues;
+
+        protected void onPreExecute(){
+            CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+            drinkValues = new ContentValues();
+            drinkValues.put("FAVORITE", favorite.isChecked());
+        }
+
+        protected Boolean doInBackground(Integer... drinks){
+            int drinkId = drinks[0];
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarBuzzDatabaseHelper(DrinkActivity.this);
+            try {
+                SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+                db.update("DRINK",
+                        drinkValues,
+                        "_id = ?",
+                        new String[]{Integer.toString(drinkId)});
+                db.close();
+                return true;
+            } catch(SQLiteException e){
+                return false;
+            }
+
+        }
+
+        protected void onPostExecute(Boolean success){
+            if(!success){
+                Toast toast = Toast.makeText(DrinkActivity.this,
+                        "Database unavailable", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
     }
 }
